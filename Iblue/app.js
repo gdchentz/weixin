f219c6b65a2b900531fdfefaae980c02e15ca129
gdchentz <35604981@qq.com>
2025-07-18 10:58:56 +0800

    re establishe workfiles

---
+++ Iblue/app.js
@@ -0,0 +1,8 @@
+// app.js
+App({
+  globalData: {
+    serviceId: '6E400003-B5A3-F393-E0A9-E50E24DCCA9E',
+    writeCharId: '6E400003-B5A3-F393-E0A9-E50E24DCCA9E',
+    notifyCharId: '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
+  }
+})
