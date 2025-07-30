896e3bd6a3d4a6ac666aea1d11508089ea3de2d9
35604981 <35604981@qq.com>
2025-07-31 01:19:15 +0800

    modifed files

--- Iblue/pages/ble/index.js
+++ Iblue/pages/ble/index.js
@@ -9,6 +9,7 @@ Page({
     connected: false,
     connecting: false,
     receivedData: "",
+    sendData:"testtext",
     currentDevice: null,
     log: [],
     serviceId: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E",
@@ -461,8 +462,9 @@ Page({
       this.handleError("❌ 发送失败", { errMsg: "蓝牙参数无效" });
       return;
     }
-    
-    const data = "AABBCCDD"; // 示例数据
+    // const data = "aafjkadjfa"; // 示例数据
+    const data = this.data.sendData; // 示例数据
+    // this.log(`发送数据：${this.data.sendData}`)
     wx.writeBLECharacteristicValue({
       deviceId,
       serviceId,
